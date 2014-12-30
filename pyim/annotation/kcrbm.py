ce4229361cf4caf1ae2c9c0e2dce43bb42020d4b
Julian de Ruiter <j.r.deruiter@icloud.com>
2014-12-30 08:59:14 +0100

    Updated imports, workaround for libreadline.

--- pyim/annotation/kcrbm.py
+++ pyim/annotation/kcrbm.py
@@ -1,19 +1,26 @@
+
+import readline  # Work-around for: ../lib/libreadline.so.6: undefined symbol: PC
+
 import pandas
 import pandas.rpy.common as com
-
 from numpy import issubdtype
 from rpy2 import robjects
 from rpy2.robjects.packages import importr
 
+from pyim_common.util.list import repeat_list, flatten_list
+
 from pyim.annotation.annotator import Annotator
-from pyim.common.util import repeat_list, flatten_list
 
 
 class KcrbmAnnotator(Annotator):
 
     def __init__(self, genome, system):
+        super(KcrbmAnnotator, self).__init__()
         if system not in {'SB'}:
             raise ValueError('Unknown system {}'.format(system))
+
+        if genome not in {'mm10'}:
+            raise ValueError('Unsupported genome {}'.format(genome))
 
         self.genome = genome
         self.system = system
@@ -28,24 +35,6 @@ class KcrbmAnnotator(Annotator):
         kcrbm_result = self._run_kcrbm(kcrbm_ins, method='transcripts')
         return self._parse_transcript_result(kcrbm_result)
 
-    def _convert_to_kcrbm_frame(self, insertions):
-        ## Extract and rename required columns.
-        kcrbm_frame = insertions.ix[:,['id', 'seqname', 'location', 'strand']]
-        kcrbm_frame.columns = ['id', 'chr', 'base', 'ori']
-
-        ## Map chromosomes to numeric values.
-        chr_map = dict(zip(
-            list(map(str, range(1, 19+1))) + ['X', 'Y'],
-            range(1, 21+1)
-        ))
-        kcrbm_frame['chr'] = kcrbm_frame['chr'].map(chr_map).astype(int)
-
-        ## Convert orientation if required.
-        if not issubdtype(kcrbm_frame['ori'].dtype, int):
-            kcrbm_frame['ori'] = kcrbm_frame['ori'].map({'+': 1, '-': -1})
-
-        return kcrbm_frame
-
     def _run_kcrbm(self, kcrbm_frame, method):
         kcrbm_frame['ins_id'] = kcrbm_frame['id']
         ins_kcrbm_r = com.convert_to_r_dataframe(kcrbm_frame)
@@ -57,14 +46,27 @@ class KcrbmAnnotator(Annotator):
 
         return com.convert_robj(res)
 
-    def _parse_gene_result(self, result):
-        result = result.ix[result['ensid'].astype(str) != 'NA']
-        return pandas.DataFrame({'id': result['ins_id'],
-                                 'gene': result['ensid'],
-                                 'mechanism': result['mechanism']},
-                                columns=['id', 'gene', 'mechanism'])
+    @staticmethod
+    def _convert_to_kcrbm_frame(insertions):
+        # Extract and rename required columns.
+        kcrbm_frame = insertions.ix[:, ['id', 'seqname', 'location', 'strand']]
+        kcrbm_frame.columns = ['id', 'chr', 'base', 'ori']
 
-    def _load_genome(self, genome):
+        # Map chromosomes to numeric values.
+        chr_map = dict(zip(
+            list(map(str, range(1, 19 + 1))) + ['X', 'Y'],
+            range(1, 21 + 1)
+        ))
+        kcrbm_frame['chr'] = kcrbm_frame['chr'].map(chr_map).astype(int)
+
+        # Convert orientation if required.
+        if not issubdtype(kcrbm_frame['ori'].dtype, int):
+            kcrbm_frame['ori'] = kcrbm_frame['ori'].map({'+': 1, '-': -1})
+
+        return kcrbm_frame
+
+    @staticmethod
+    def _load_genome(genome):
         utils = importr("utils")
 
         if genome == 'mm10':
@@ -75,7 +77,16 @@ class KcrbmAnnotator(Annotator):
 
         return genome_obj
 
-    def _parse_transcript_result(self, result):
+    @staticmethod
+    def _parse_gene_result(result):
+        result = result.ix[result['ensid'].astype(str) != 'NA']
+        return pandas.DataFrame({'id': result['ins_id'],
+                                 'gene': result['ensid'],
+                                 'mechanism': result['mechanism']},
+                                columns=['id', 'gene', 'mechanism'])
+
+    @staticmethod
+    def _parse_transcript_result(result):
         result = result.ix[result['ensid'].astype(str) != 'NA']
 
         tr_list = result['transid'].str.split('|')
@@ -89,4 +100,4 @@ class KcrbmAnnotator(Annotator):
         return pandas.DataFrame({'id': ins_id, 'gene': ens_id,
                                  'transcript': flatten_list(tr_list),
                                  'mechanism': flatten_list(mech_list)},
-                                 columns=['id', 'gene', 'transcript', 'mechanism'])
+                                columns=['id', 'gene', 'transcript', 'mechanism'])
