308015c9d311a3282d78e214f4d895bc362d16d2
Julian de Ruiter <julianderuiter@gmail.com>
2015-12-23 23:08:18 +0100

    Add level name and command to logging.

--- pyim/main/_logging.py
+++ pyim/main/_logging.py
@@ -3,16 +3,21 @@ import pkg_resources
 
 
 logging.basicConfig(
-        format='%(asctime)-15s %(message)s',
+        format='%(asctime)-15s %(levelname)-10s %(message)s',
         datefmt='[%Y-%m-%d %H:%M:%S]',
         level=logging.INFO)
 
 
-def print_header(logger):
+def print_header(logger, command=None):
     version = pkg_resources.require('pyim')[0].version
-    header_str = ' PyIM ({}) '.format(version)
-    logger.info('{:-^40}'.format(header_str))
+
+    if command is None:
+        header_str = ' PyIM ({}) '.format(version)
+    else:
+        header_str = ' PyIM {} ({}) '.format(command, version)
+
+    logger.info('{:-^60}'.format(header_str))
 
 
 def print_footer(logger):
-    logger.info('{:-^40}'.format(' Done! '))
+    logger.info('{:-^60}'.format(' Done! '))
