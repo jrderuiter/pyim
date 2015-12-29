8217d752ccf790cab7981446e50fa1a29de6660e
Julian de Ruiter <julianderuiter@gmail.com>
2015-12-29 09:39:20 +0100

    Updated pyim-gff for column name changes.

--- pyim/main/gff.py
+++ pyim/main/gff.py
@@ -6,7 +6,6 @@ from builtins import (ascii, bytes, chr, dict, filter, hex, input,
 from future.utils import native_str
 
 from argparse import ArgumentParser
-from pathlib import Path
 
 import pandas as pd
 
@@ -14,8 +13,8 @@ import pandas as pd
 def setup_parser():
     parser = ArgumentParser(prog='pyim-gff')
 
-    parser.add_argument('insertions', type=Path)
-    parser.add_argument('output_gff', type=Path)
+    parser.add_argument('insertions')
+    parser.add_argument('output')
 
     return parser
 
@@ -24,22 +23,22 @@ def _ins_to_gff(ins, size=1000):
     assert isinstance(ins.strand, int)
 
     attrs = [i for i in ins.index if i not in
-             {'insertion_id', 'seqname', 'location', 'strand'}]
+             {'id', 'seqname', 'location', 'strand'}]
 
     attr_dict = {attr: ins[attr] for attr in attrs}
 
-    attr_dict['id'] = ins.insertion_id
-    attr_dict['name'] = ins.insertion_id
+    attr_dict['id'] = ins['id']
+    attr_dict['name'] = ins['id']
 
     attr_keys = sorted(attr_dict.keys())
     attr_str = ';'.join(('{} {}'.format(k, attr_dict[k]) for k in attr_keys))
 
     return {
-        'seqname': ins.seqname,
+        'seqname': ins['chrom'],
         'source': '.',
         'feature': 'insertion',
-        'start': int(ins.location - (size / 2)),
-        'end': int(ins.location + (size / 2)),
+        'start': int(ins['position'] - (size / 2)),
+        'end': int(ins['position'] + (size / 2)),
         'score': '.',
         'strand': '+' if ins.strand == 1 else '-',
         'frame': '.',
@@ -52,18 +51,18 @@ def main():
     args = parser.parse_args()
 
     # Read input.
-    ins_frame = pd.read_csv(str(args.insertions), sep=native_str('\t'),
-                            dtype={'seqname': str, 'location': int})
+    ins_frame = pd.read_csv(args.insertions, sep=native_str('\t'),
+                            dtype={'chrom': str, 'position': int})
 
     # Transform to gff frame.
     gff_frame = pd.DataFrame.from_records(
         (_ins_to_gff(r) for _, r in ins_frame.iterrows()),
         columns=['seqname', 'source', 'feature', 'start', 'end',
                  'score', 'strand', 'frame', 'attribute'])
-    gff_frame = gff_frame.sort(['seqname', 'start', 'end'])
+    gff_frame = gff_frame.sort_values(by=['seqname', 'start', 'end'])
 
     # Write output.
-    gff_frame.to_csv(str(args.output_gff), sep=native_str('\t'),
+    gff_frame.to_csv(args.output, sep=native_str('\t'),
                      index=False, header=False)
 
 
